c8b08e7866b1680f9b737e76bf8582e0459bb7ae
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-29 23:09:16 +0100

    Update serpa.c

--- serpa.c
+++ serpa.c
@@ -1,123 +1,128 @@
 #include "main.h"
-
-/**
- * print_string_custom - Prints a string with custom conversion specifier %S
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_string_custom(va_list args, int *count)
+/***************************************
+ * @s: format string
+ * get_specifier - finds the format of function
+ * Return: number of bytes
+ ***************************************/
+int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
-	int i, len = 0;
-	char *str = va_arg(args, char *);
-
-	if (str == NULL)
-		str = "(null)";
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if ((str[i] > 0 && str[i] < 32) || str[i] >= 127)
-		{
-			*count += charwrite('\\', count);
-			*count += charwrite('x', count);
-			*count += charwrite('0', count);
-			*count += charwrite('0', count);
-			*count += print_hex_upper_single(str[i], count);
-			len += 5;
-		}
-		else
-		{
-			*count += charwrite(str[i], count);
-			len++;
-		}
-	}
-
-	return (len);
-}
-
-/**
- * print_pointer - Prints a pointer address
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_pointer(va_list args, int *count)
-{
-	unsigned long int ptr = (unsigned long int)va_arg(args, void *);
-	char *hex = "0123456789abcdef";
-	int digits = 0;
-
-	*count += charwrite('0', count);
-	*count += charwrite('x', count);
-
-	if (ptr == 0)
-	{
-		*count += charwrite('0', count);
-		return (3);
-	}
-
-	while (ptr != 0)
-	{
-		*count += charwrite(hex[ptr % 16], count);
-		ptr /= 16;
-		digits++;
-	}
-
-	return (digits + 3);
-}
-
-/**
- * print_reverse - Prints a string in reverse
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_reverse(va_list args, int *count)
-{
+	specifier_t specifiers[] = {
+		
+		{"d", print_int},
+		{"i", print_int},
+		{"s", print_string},
+		{"r", print_rev},
+		{"b", print_binary},
+		{"S", print_S},
+		{"o", print_octal},
+		{"R", print_rot13},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"%", print_percent},
+		{"c", print_char},
+		{"u", print_unsigned},
+		{NULL, NULL}
+	};
 	int i;
-	char *str = va_arg(args, char *);
-	int len = _strlen(str);
 
-	for (i = len - 1; i >= 0; i--)
-		*count += charwrite(str[i], count);
-
-	return (len);
-}
-
-/**
- * print_rot13 - Prints a string in ROT13 encoding
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_rot13(va_list args, int *count)
-{
-	int i, j;
-	char *str = va_arg(args, char *);
-	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int len = _strlen(str);
-	int converted = 0;
-
-	for (i = 0; i < len; i++)
+	for (i = 0; specifiers[i].specifier; i++)
 	{
-		converted = 0;
-		for (j = 0; j < 52; j++)
-		{
-			if (str[i] == alpha[j])
-			{
-				*count += charwrite(rot13[j], count);
-				converted = 1;
-				break;
-			}
-		}
-		if (!converted)
-			*count += charwrite(str[i], count);
+    		if (*s == specifiers[i].specifier[0])
+    		{
+        		return specifiers[i].f;
+    		}
 	}
+	
+	return (NULL);
+}
+/****************************************
+ * gpf - finds format function
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * @s: the format string
+ * Return: number of bytes
+ ****************************************/
+int gpf(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
 
-	return (len);
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+/********************************
+ * @s: format string
+ * @params:parameters struct
+ * gf - finds flag function
+ * Return: if flag was valid
+ *******************************/
+int gf(char *s, params_t *params)
+{
+	int a = 0;
+
+	switch (*s)
+	{
+		case '+':
+			a = params->plus_f = 1;
+			break;
+		case ' ':
+			a = params->space_f = 1;
+			break;
+		case '#':
+			a = params->hashtag_f = 1;
+			break;
+		case '-':
+			a = params->minus_f = 1;
+			break;
+		case '0':
+			a = params->zero_f = 1;
+			break;
+	}
+	return (a);
+}
+/*************************************
+ * gm - getsmodifier function
+ * @params: the parameters struct
+ * @s: the format string
+ * Return: if modifier was valid
+ ************************************/
+int gm(char *s, params_t *params)
+{
+	int m = 0;
+
+	switch (*s)
+	{
+	case 'h':
+		m = params->h_m = 1;
+		break;
+	case 'l':
+		m = params->l_m = 1;
+		break;
+	}
+	return (m);
+}
+/***************************************************
+ * @params: parameters struct
+ * gw - gets width from format string
+ * @ap: argument pointer
+ * @s: format string
+ * Return: new pointer
+ ***************************************************/
+char *gw(char *s, params_t *params, va_list ap)
+{
+	int r = 0;
+
+	if (*s == '*')
+	{
+		r = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			r = r * 10 + (*s++ - '0');
+	}
+	params->w = r;
+	return (s);
 }
