2ea4880c39b6663d72aa119b5dde54e62d4dd246
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-30 00:41:41 +0100

    Update my_prinf.c

--- my_prinf.c
+++ my_prinf.c
@@ -1,36 +1,122 @@
 #include "main.h"
-/**
- * _printf - print formatted string
- * @format: string
- * Return: to count of characs printed
- */
-int _printf(const char *format, ...)
+/*********************************
+ * @ap: argument pointer
+ * @params: parameters struct
+ * pc - prints character
+ * Return: number chars printed
+ *********************************/
+int pc(va_list ap, params_t *params)
 {
-	int printed_chars;
-	Conversion conversion_list[] = {
-		{"c", print_character},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rotate_13},
-		{"u", print_unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hexadecimal},
-		{"X", print_HEXADECIMAL},
-		{NULL, NULL}
-	};
-	va_list arg_list;
+	char p_c = ' ';
+	unsigned int p = 1, sum = 0, nv = va_arg(ap, int);
 
-	if (format == NULL)
-		return (-1);
-
-	va_start(arg_list, format);
-	printed_chars = parse_format(format, conversion_list, arg_list);
-	va_end(arg_list);
-
-	return (printed_chars);
+	if (params->minus_f)
+		sum += _putchar(nv);
+	while (p++ < params->width)
+		sum += _putchar(p_c);
+	if (!params->minus_f)
+		sum += _putchar(nv);
+	return (sum);
 }
+/********************************
+ * @ap: argument pointer
+ * @params: the parameters struct
+ * pp - prints string
+ * Return: number chars printed
+ ********************************/
+int pp(va_list ap, params_t *params)
+{
+	(void)ap;
+	(void)params;
+	return (_putchar('%'));
+}
+/*******************************
+ * @params: parameters struct
+ * pi - prints integer
+ * @ap: argument pointer
+ * Return: number chars printed
+ ********************************/
+int pi(va_list ap, params_t *params)
+{
+	long j;
 
+	if (params->l_modifier)
+		j = va_arg(ap, long);
+	else if (params->h_modifier)
+		j = (short int)va_arg(ap, int);
+	else
+		j = (int)va_arg(ap, int);
+	return (print_number(convert(j, 10, 0, params), params));
+}
+/*********************************
+ * @params: the parameters struct
+ * p_S - custom format specifier
+ * @ap: argument pointer
+ * Return: number chars printed
+ ********************************/
+int p_S(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *);
+	char *hex;
+	int sum = 0;
+
+	if ((int)(!str))
+		return (_puts(NULL_STRING));
+	for (; *str; str++)
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			sum += _putchar('\\');
+			sum += _putchar('x');
+			hex = convert(*str, 16, 0, params);
+			if (!hex[1])
+				sum += _putchar('0');
+			sum += _puts(hex);
+		}
+		else
+		{
+			sum += _putchar(*str);
+		}
+	}
+	return (sum);
+}
+/*********************************
+ * @params: parameters struct
+ * pst - prints string
+ * @ap: argument pointer
+ * Return: number chars printed
+ *********************************/
+int pst(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *), p_c = ' ';
+	unsigned int p = 0, sum = 0, h = 0, y;
+
+	(void)params;
+	switch ((int)(!str))
+		case 1:
+			str = NULL_STRING;
+
+	y = p = _strlen(str);
+	if (params->precision < p)
+		y = p = params->precision;
+
+	if (params->minus_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (h = 0; h < p; h++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	while (y++ < params->width)
+		sum += _putchar(p_c);
+	if (!params->minus_f)
+	{
+		if (params->precision != UINT_MAX)
+			for (h = 0; h < p; h++)
+				sum += _putchar(*str++);
+		else
+			sum += _puts(str);
+	}
+	return (sum);
+}
