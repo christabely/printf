d9bf6d6a39fdd27ce478683c7c2ea915ecb096a4
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-29 23:36:01 +0100

    Update num_p.c

--- num_p.c
+++ num_p.c
@@ -1,215 +1,74 @@
 #include "main.h"
 
-/**
- * print_decimal - Prints a decimal number
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_decimal(va_list args, int *count)
+/**************************************************
+ * p_f_t - prints a range of char addresses
+ * @except: except address
+ * @start: starting address
+ * @stop: stopping address
+ * Return: number bytes
+ ***************************************************/
+int p_f_t(char *start, char *stop, char *except)
 {
-	int num = va_arg(args, int);
-	int digits = 0;
-	int sign = 0;
+	int sum = 0;
 
-	if (num == 0)
+	while (start <= stop)
 	{
-		*count += charwrite('0', count);
-		return (1);
+		if (start != except)
+			sum += _putchar(*start);
+		start++;
 	}
-
-	if (num < 0)
-	{
-		sign = 1;
-		num = -num;
-		*count += charwrite('-', count);
-	}
-
-	while (num > 0)
-	{
-		*count += charwrite((num % 10) + '0', count);
-		num /= 10;
-		digits++;
-	}
-
-	return (digits + sign);
+	return (sum);
 }
-
-/**
- * print_unsigned - Prints an unsigned integer
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_unsigned(va_list args, int *count)
+/**************************************
+ * @ap: string
+ * @params: the parameters struct
+ * pro - prints string in rot13
+ * Return: number bytes printed
+ **************************************/
+int pro(va_list ap, params_t *params)
 {
-	unsigned int num = va_arg(args, unsigned int);
-	int digits = 0;
+	int m, d;
+	int c = 0;
+	char tfs[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(ap, char *);
+	(void)params;
 
-	if (num == 0)
+	m = 0;
+	d = 0;
+	while (a[m])
 	{
-		*count += charwrite('0', count);
-		return (1);
-	}
-
-	while (num > 0)
-	{
-		*count += charwrite((num % 10) + '0', count);
-		num /= 10;
-		digits++;
-	}
-
-	return (digits);
-}
-
-/**
- * print_binary - Prints a binary number
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_binary(va_list args, int *count)
-{
-	unsigned int num = va_arg(args, unsigned int);
-	int digits = 0;
-	int binary[32];
-
-	if (num == 0)
-	{
-		*count += charwrite('0', count);
-		return (1);
-	}
-
-	while (num > 0)
-	{
-		binary[digits] = num % 2;
-		num /= 2;
-		digits++;
-	}
-
-	while (digits > 0)
-	{
-		*count += charwrite(binary[digits - 1] + '0', count);
-		digits--;
-	}
-
-	return (digits);
-}
-
-/**
- * print_octal - Prints an octal number
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_octal(va_list args, int *count)
-{
-	unsigned int num = va_arg(args, unsigned int);
-	int digits = 0;
-	int octal[32];
-
-	if (num == 0)
-	{
-		*count += charwrite('0', count);
-		return (1);
-	}
-
-	while (num > 0)
-	{
-		octal[digits] = num % 8;
-		num /= 8;
-		digits++;
-	}
-
-	while (digits > 0)
-	{
-		*count += charwrite(octal[digits - 1] + '0', count);
-		digits--;
-	}
-
-	return (digits);
-}
-
-/**
- * print_hex - Prints a hexadecimal number (lowercase)
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_hex(va_list args, int *count)
-{
-	unsigned int num = va_arg(args, unsigned int);
-	int digits = 0;
-	char hex[32];
-	int rem;
-
-	if (num == 0)
-	{
-		*count += charwrite('0', count);
-		return (1);
-	}
-
-	while (num > 0)
-	{
-		rem = num % 16;
-		if (rem < 10)
-			hex[digits] = rem + '0';
+		if ((a[m] >= 'A' && a[m] <= 'Z')
+		    || (a[m] >= 'a' && a[m] <= 'z'))
+		{
+			d = a[m] - 65;
+			c += _putchar(tfs[d]);
+		}
 		else
-			hex[digits] = (rem - 10) + 'a';
-		num /= 16;
-		digits++;
+			c += _putchar(a[m]);
+		m++;
 	}
-
-	while (digits > 0)
-	{
-		*count += charwrite(hex[digits - 1], count);
-		digits--;
-	}
-
-	return (digits);
+	return (c);
 }
-
-/**
- * print_hex_upper - Prints a hexadecimal number (uppercase)
- * @args: The va_list argument
- * @count: Pointer to the counter of characters printed
- *
- * Return: The number of characters printed
- */
-int print_hex_upper(va_list args, int *count)
+/**************************************
+ * @ap: string
+ * @params: parameters struct
+ * pr - prints string in reverse
+ * Return: number bytes
+ **************************************/
+int pr(va_list ap, params_t *params)
 {
-	unsigned int num = va_arg(args, unsigned int);
-	int digits = 0;
-	char hex[32];
-	int rem;
+	int len, sum = 0;
+	char *str = va_arg(ap, char *);
+	(void)params;
 
-	if (num == 0)
+	if (str)
 	{
-		*count += charwrite('0', count);
-		return (1);
+		for (len = 0; *str; str++)
+			len++;
+		str--;
+		for (; len > 0; len--, str--)
+			sum += _putchar(*str);
 	}
-
-	while (num > 0)
-	{
-		rem = num % 16;
-		if (rem < 10)
-			hex[digits] = rem + '0';
-		else
-			hex[digits] = (rem - 10) + 'A';
-		num /= 16;
-		digits++;
-	}
-
-	while (digits > 0)
-	{
-		*count += charwrite(hex[digits - 1], count);
-		digits--;
-	}
-
-	return (digits);
+	return (sum);
 }
