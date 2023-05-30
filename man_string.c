29e13e6a672105600345a337dcae2b500337035e
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-30 05:36:45 +0100

    Update man_string.c

--- man_string.c
+++ man_string.c
@@ -1,24 +1,22 @@
 #include "main.h"
+
+void itp(params_t *par, va_list pr)
+{
+par->p = UINT_MAX;
+par->w = 0;
+par->unsign = 0;
+par->hashtag_f = 0;
+par->zero_f = 0;
+par->plus_f = 0;
+par->minus_f = 0;
+par->space_f = 0;
+params->l_m = 0;
+params->h_m = 0;
+(void)pr;
+}
 /*************************************************
- * @ap: argument pointer
- * init_params - clears struct fields and reset buf
- * @params: parameters struct
+ * @pr: argument pointer
+ * itp - clears struct fields and reset buf
+ * @par: parameters struct
  * Return: void
  *************************************************/
-void init_params(params_t *params, va_list ap)
-{
-	params->unsign = 0;
-
-	params->hashtag_f = 0;
-	params->zero_f = 0;
-	params->plus_f = 0;
-	params->minus_f = 0;
-	params->space_f = 0;
-
-	params->precision = UINT_MAX;
-	params->width = 0;
-
-	params->l_m = 0;
-	params->h_m = 0;
-	(void)ap;
-}
