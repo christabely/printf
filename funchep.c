7d217c0cd26053054c26743377c82f0f703838ed
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-30 01:26:33 +0100

    Update funchep.c

--- funchep.c
+++ funchep.c
@@ -1,37 +1,100 @@
 #include "main.h"
-
-/**
- * funchep - Returns a function pointer based on the conversion specifier
- * @format: The conversion specifier
- *
- * Return: Pointer to the corresponding conversion function, or NULL if invalid.
- */
-int (*funchep(const char *format))(va_list, int *)
+/*******************************************************
+ * p_h - prints unsigned lowercase hex num
+ * @ap: argument pointer
+ * @params: parameters struct
+ * Return: bytes printed
+ *******************************************************/
+int p_h(va_list ap, params_t *params)
 {
-	int i;
-	converter_t converters[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_decimal},
-		{'i', print_decimal},
-		{'b', print_binary},
-		{'u', print_unsigned},
-		{'o', print_octal},
-		{'x', print_hex},
-		{'X', print_hex_upper},
-		{'S', print_string_custom},
-		{'p', print_pointer},
-		{'r', print_reverse},
-		{'R', print_rot13},
-		{'\0', NULL}
-	};
+	unsigned long h;
+	int c = 0;
+	char *str;
 
-	for (i = 0; converters[i].specifier != '\0'; i++)
+	if (params->h_m)
+		h = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_m)
+		h = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		h = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(h, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	if (params->hashtag_flag && l)
 	{
-		if (converters[i].specifier == *format)
-			return (converters[i].printer);
+		*--str = 'x';
+		*--str = '0';
 	}
+	params->unsign = 1;
+	return (c += pn(str, params));
+}
+/************************************************
+ * po - prints unsigned octal numbers
+ * @ap: the argument pointer
+ * @params: the parameters struct
+ * Return: bytes printed
+ ************************************************/
+int po(va_list ap, params_t *params)
+{
+	unsigned long k;
+	char *str;
+	int c = 0;
 
-	return (NULL);
+	if (params->h_modifier)
+		k = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_m)
+		k = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		k = (unsigned int)va_arg(ap, unsigned int);
+	str = convert(k, 8, CONVERT_UNSIGNED, params);
+
+	if (params->hashtag_f && k)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += p_n(str, params));
+}
+/*********************************************************
+ * @ap: argument pointer
+ * @params: parameters struct
+ * p_H - prints unsigned hex numbers in uppercase
+ * Return: bytes printed
+ *********************************************************/
+int p_H(va_list ap, params_t *params)
+{
+	unsigned long h;
+	int c = 0;
+	char *str;
+
+	if (params->l_m)
+		h = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_m)
+		h = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		h = (unsigned int)va_arg(ap, unsigned int);
+
+	str = convert(h, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += pn(str, params));
+}
+/**********************************************
+ * print_binary - prints unsigned binary number
+ * @ap: the argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+ **********************************************/
+int print_binary(va_list ap, params_t *params)
+{
+	unsigned int u = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+	int q = 0;
+
+	if (params->hashtag_f && u)
+		*--str = '0';
+	params->unsign = 1;
+	return (q += p_n(str, params));
 }
