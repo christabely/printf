168de65be2bdc7111ed39512a6e10127170af5ff
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-30 04:13:49 +0100

    Update num_p.c

--- num_p.c
+++ num_p.c
@@ -1,74 +1,73 @@
 #include "main.h"
-
 /**************************************************
- * p_f_t - prints a range of char addresses
- * @except: except address
- * @start: starting address
- * @stop: stopping address
+ * pft - prints range of char addresses
+ * @x: except address
+ * @z: start address
+ * @s: stop address
  * Return: number bytes
  ***************************************************/
-int p_f_t(char *start, char *stop, char *except)
+int pft(char *z, char *s, char *x)
 {
-	int sum = 0;
+int sum = 0;
 
-	while (start <= stop)
-	{
-		if (start != except)
-			sum += _putchar(*start);
-		start++;
-	}
-	return (sum);
+while (z <= s)
+{
+if (z != x)
+sum += _putchar(*z);
+z++;
+}
+return (sum);
 }
 /**************************************
- * @ap: string
- * @params: the parameters struct
+ * @pr: string
+ * @par: parameters struct
  * pro - prints string in rot13
  * Return: number bytes printed
  **************************************/
-int pro(va_list ap, params_t *params)
+int pro(va_list pr, params_t *par)
 {
-	int m, d;
-	int c = 0;
-	char tfs[] =
-		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
-	char *a = va_arg(ap, char *);
-	(void)params;
+int u, index;
+int c = 0;
+char Phill[] =
+"BCDEFGHIJKLMNOPQRSTUVWXYZA	bcdefghijklmnopqrstuvwxyza";
+char *p = va_arg(pr, char *);
+(void)par;
 
-	m = 0;
-	d = 0;
-	while (a[m])
-	{
-		if ((a[m] >= 'A' && a[m] <= 'Z')
-		    || (a[m] >= 'a' && a[m] <= 'z'))
-		{
-			d = a[m] - 65;
-			c += _putchar(tfs[d]);
-		}
-		else
-			c += _putchar(a[m]);
-		m++;
-	}
-	return (c);
+u = 0;
+index = 0;
+while (p[u])
+{
+if ((p[u] >= 'A' && p[u] <= 'Z')
+|| (p[u] >= 'a' && p[u] <= 'z'))
+{
+index = p[u] - 65;
+c += _putchar(Phill[index]);
+}
+else
+c += _putchar(p[u]);
+u++;
+}
+return (c);
 }
 /**************************************
- * @ap: string
- * @params: parameters struct
- * pr - prints string in reverse
+ * @pr: string
+ * @par: parameters struct
+ * prr - prints string in reverse
  * Return: number bytes
  **************************************/
-int pr(va_list ap, params_t *params)
+int prr(va_list pr, params_t *par)
 {
-	int len, sum = 0;
-	char *str = va_arg(ap, char *);
-	(void)params;
+int lh, sum = 0;
+char *sr = va_arg(pr, char *);
+(void)par;
 
-	if (str)
-	{
-		for (len = 0; *str; str++)
-			len++;
-		str--;
-		for (; len > 0; len--, str--)
-			sum += _putchar(*str);
-	}
-	return (sum);
+if (sr)
+{
+for (lh = 0; *sr; sr++)
+lh++;
+str--;
+for (; lh > 0; lh--, sr--)
+sum += _putchar(*sr);
+}
+return (sum);
 }
