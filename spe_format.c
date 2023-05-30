3a40c460137d0984eb861c9924242fd5f80f090e
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-30 02:12:20 +0100

    Update spe_format.c

--- spe_format.c
+++ spe_format.c
@@ -1,32 +1,29 @@
 #include "main.h"
+
+char *gp(char *l, params_t *params, va_list ap)
+{
+int u = 0;
+
+if (*l != '.')
+return (l);
+l++;
+if (*l == '*')
+{
+u = va_arg(ap, int);
+l++;
+}
+else
+{
+while (_id(*l))
+u = u * 10 + (*l++ - '0');
+}
+params->p = u;
+return (l);
+}
 /************************************************************
  * get_precision - gets the precision from the format string
  * @l: format string
  * @ap: argument pointer
  * @params: parameters struct 
  * Return: new pointer
- *************************************************************/
-char *gp(char *l, params_t *params, va_list ap)
-{
-	int u = 0;
-
-	if (*l == '.')
-	{
-		l++;
-		if (*l == '*')
-		{
-			u = va_arg(ap, int);
-			l++;
-		}
-		else
-		{
-			while (_isdigit(*l))
-			{
-				u = u * 10 + (*l - '0');
-				l++;
-			}
-		}
-		params->p = u;
-	}
-	return (l);
-}
+ ************************************************************/
