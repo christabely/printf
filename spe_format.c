e4f18785b7eac1e8869c7817e89ec84320c339dc
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-30 17:02:04 +0100

    Update spe_format.c

--- spe_format.c
+++ spe_format.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * gp - gets precision from format string
+ * gp - get precision from format string
  * @z: base num as string
  * @pr: pointer argument
  * @par: parameters struct
