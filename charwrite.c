57709628de3b40b1be97f8e644bda84cfd2df5fb
Mzpenelope <129563638+Mzpenelope@users.noreply.github.com>
2023-05-30 01:31:35 +0100

    Update charwrite.c

--- charwrite.c
+++ charwrite.c
@@ -1,13 +1,34 @@
 #include "main.h"
-
-/**
- * charwrite - Writes a character to the standard output
- * @c: The character to write
- * @count: Pointer to the counter of characters printed
- *
- * Return: On success, 1. On error, -1.
- */
-int charwrite(char c, int *count)
+/***************************************
+ * @str: string to print
+ * _puts - prints string with newline
+ * Return: void
+ ***************************************/
+int _puts(char *str)
 {
-	return (write(1, &c, 1));
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+/**********************************************************
+ * _putchar - writes char c to stdout
+ * @c: character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ **********************************************************/
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
